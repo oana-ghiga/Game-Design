b36d7bc7fe3eddf915d15d23cb6d0e3c81cb2c10
oana-ghiga <oana.r.maria@gmail.com>
2024-04-03 13:36:28 +0300

    .

---
+++ Magic/Assets/scripts/AudioManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    [SerializeField] AudioSource musicSource;
+    [SerializeField] AudioSource sfxSource;
+    
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void PlaySFX(AudioClip clip)
+    {
+        sfxSource.PlayOneShot(clip);
+    }
+
+    public void PlayMusic(AudioClip clip)
+    {
+        musicSource.clip = clip;
+        musicSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        musicSource.Stop();
+    }
+}
